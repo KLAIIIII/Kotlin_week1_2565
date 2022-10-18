23492cb4d4b865d8d646bc217d2ecf79ef973562
KLAIIIII <guyyg03546@gmail.com>
2022-10-18 17:42:20 +0700

    Update

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main() {
+
+    kDataType()
+
+    println("#Any Type \n ==============")
+    val kAny1: Any = 20
+    val kAny2: Any = 100.4
+    val kAny3: Any = "Mark Zuckerberg"
+    println("kAny1: $kAny1")
+    println("kAny2: $kAny2")
+    println("kAny3: $kAny3")
+
+//    val a = 10 ; val b = 5 ; val c = a+b
+//    println(c)
+//    println("Hello Kotlin!")
+//    println("สวัสดีชาวคอทลิน!")
+//    println(5+5)
+//    println("วันนี้ขอให้เป็นวันที่น่ารักและทำให้คุณใจฟูนะครับ" + " To my cute")
+//    print("เข้าสู่การเขียนโปรแกรม \n")
+//    print("\t Enjoy!")
+}
+
